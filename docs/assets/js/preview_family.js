4a9799e2ee5cf5193317dc676377cd6396210942
Ivan Herman <ivan@ivan-herman.net>
2025-02-20 14:13:59 +0100

    Updated the epub viewer

--- docs/assets/js/preview_family.js
+++ docs/assets/js/preview_family.js
@@ -60,57 +60,57 @@ var repo_name = "epub-specs";
 var repo_owner = "w3c";
 var parts = [
   {
-    path: "epub33/overview/index.html",
+    path: "epub34/overview/index.html",
     title: "EPUB 3 Overview",
     short_name: "overview"
   },
   {
-    path: "epub33/core/index.html",
-    title: "EPUB 3.3",
+    path: "epub34/authoring/index.html",
+    title: "EPUB 3.4",
     short_name: "core"
   },
   {
-    path: "epub33/rs/index.html",
-    title: "EPUB 3.3 Reading Systems",
+    path: "epub34/rs/index.html",
+    title: "EPUB 3.4 Reading Systems",
     short_name: "rs"
   },
   {
-    path: "epub33/multi-rend/index.html",
+    path: "epub34/multi-rend/index.html",
     title: "EPUB Multiple-Rendition Publications 1.1",
     short_name: "multi-rend"
   },
   {
-    path: "epub33/tts/index.html",
+    path: "epub34/tts/index.html",
     title: "EPUB 3 Text-to-Speech Enhancements",
     short_name: "tts"
   },
   {
-    path: "epub33/a11y/index.html",
-    title: "EPUB Accessibility 1.1",
+    path: "epub34/a11y/index.html",
+    title: "EPUB Accessibility 1.2",
     short_name: "a11y"
   },
   {
-    path: "epub33/a11y-tech/index.html",
+    path: "epub34/a11y-tech/index.html",
     title: "EPUB Accessibility Techniques",
     short_name: "a11y-tech"
   },
   {
-    path: "epub33/epub-a11y-eaa-mapping/index.html",
+    path: "epub34/epub-a11y-eaa-mapping/index.html",
     title: "EPUB Accessibility - EU Accessibility Act Mapping",
     short_name: "epub-a11y-eaa-mapping"
   },
   {
-    path: "epub33/a11y-exemption/index.html",
+    path: "epub34/a11y-exemption/index.html",
     title: "The EPUB Accessibility exemption property",
     short_name: "a11y-exemption"
   },
   {
-    path: "epub33/fxl-a11y/index.html",
+    path: "epub34/fxl-a11y/index.html",
     title: "EPUB Fixed Layout Accessibility",
     short_name: "fxl-a11y"
   },
   {
-    path: "epub33/fxl-a11y-tech/index.html",
+    path: "epub34/fxl-a11y-tech/index.html",
     title: "EPUB Fixed Layout Accessibility Techniques",
     short_name: "fxl-a11y-tech"
   }
